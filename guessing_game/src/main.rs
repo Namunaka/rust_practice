56885ff68c693e9d842aba9ad61add07e8c475e9
galulu <namunaka@gmail.com>
2025-12-03 16:05:26 +0800

    guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("猜数游戏");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("你猜的数是: {}", guess);
 }
---
+++ guessing_game/target/debug/deps/guessing_game-593c510b123dc5a2.d
@@ -0,0 +1,5 @@
+/Users/bytedance/Documents/work/rust_practice/guessing_game/target/debug/deps/guessing_game-593c510b123dc5a2.d: src/main.rs
+
+/Users/bytedance/Documents/work/rust_practice/guessing_game/target/debug/deps/libguessing_game-593c510b123dc5a2.rmeta: src/main.rs
+
+src/main.rs:
