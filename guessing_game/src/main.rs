48f55c5979fb9ad95d244d04edbf3aa099d48c9e
galulu <namunaka@gmail.com>
2025-12-03 17:20:13 +0800

    猜一下

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
@@ -13,4 +13,10 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("你猜的数是: {}", guess);
+
+    match guess.cmp(&secret_number.to_string()) {
+        Ordering::Less => println!("太小了"),
+        Ordering::Greater => println!("太大了"),
+        Ordering::Equal => println!("恭喜你猜对了"),
+    }
 }
