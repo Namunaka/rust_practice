18fe210d7924008145a8ba71b77cb69a8b3706b2
galulu <namunaka@gmail.com>
2025-12-03 15:33:21 +0800

    rust hello world

---
+++ hellow_cargo/target/debug/deps/hellow_cargo-b71205d7e85a98ac.d
@@ -0,0 +1,5 @@
+/Users/bytedance/Documents/work/rust/hellow_cargo/target/debug/deps/hellow_cargo-b71205d7e85a98ac.d: src/main.rs
+
+/Users/bytedance/Documents/work/rust/hellow_cargo/target/debug/deps/hellow_cargo-b71205d7e85a98ac: src/main.rs
+
+src/main.rs:
---
+++ hellow_cargo/target/debug/hellow_cargo.d
@@ -0,0 +1,1 @@
+/Users/bytedance/Documents/work/rust_practice/hellow_cargo/target/debug/hellow_cargo: /Users/bytedance/Documents/work/rust_practice/hellow_cargo/src/main.rs
